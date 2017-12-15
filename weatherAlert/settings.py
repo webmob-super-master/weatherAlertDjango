a763c7174e8c0991c09551babbdcb7b8b9f9fc37
Miguel Li <limingzhe792@gmail.com>
2017-12-15 15:48:19 -0800

    edited static root

--- weatherAlert/settings.py
+++ weatherAlert/settings.py
@@ -121,8 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # default static files settings for PythonAnywhere.
 # see https://help.pythonanywhere.com/pages/DjangoStaticFiles for more info
