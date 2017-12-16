5d01de6fd0fb3213eccc2f8a3d51329c6840f484
Miguel Li <limingzhe792@gmail.com>
2017-12-15 16:00:11 -0800

    added settings module import

--- weatherAlert/urls.py
+++ weatherAlert/urls.py
@@ -13,8 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+
 
 urlpatterns = [
     url(r'^weather/', include('weatherAlert_app.urls')),
