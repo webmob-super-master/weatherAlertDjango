cc1912b3a8f391f47a3923e4536217864f9ed92b
Miguel Li <limingzhe792@gmail.com>
2018-03-28 13:12:59 -0400

    updated alert to only use RQ when more than 7 cities

--- weatherAlert_app/views.py
+++ weatherAlert_app/views.py
@@ -18,16 +18,27 @@ def alert(request, region):
     owners = Owner.objects.filter(region=requested_region.id)
     formatted_owners = ' '.join([owner.slack_handle for owner in owners])
     for i in range(0, len(cities)):
-        result = django_rq.enqueue(
-            run_task, 
-            requested_region.webhook,
-            requested_region.name,
-            cities[i].city,
-            cities[i].state,
-            formatted_owners,
-            i,
-            len(cities)
-        )
+        if i > 7:
+            result = django_rq.enqueue(
+                run_task, 
+                requested_region.webhook,
+                requested_region.name,
+                cities[i].city,
+                cities[i].state,
+                formatted_owners,
+                i,
+                len(cities)
+            )
+        else:
+            result = run_task( 
+                requested_region.webhook,
+                requested_region.name,
+                cities[i].city,
+                cities[i].state,
+                formatted_owners,
+                i,
+                len(cities)
+            )
     json_response = {
         'is_completed': True
     }
